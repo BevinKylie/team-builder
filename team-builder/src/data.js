f56a79cc712c662d36f3a07652ee8ec9b46f52ff
Bevin Richardson <bevinkylieart@gmail.com>
2020-01-22 19:44:26 -0500

    making changes

---
+++ team-builder/src/data.js
@@ -0,0 +1,21 @@
+const members = [
+    {
+        id: 1,
+        name: 'Dean Winchester',
+        role: 'Front-end Engineer',
+        image: 'https://vignette.wikia.nocookie.net/supernatural/images/d/d8/JA_SNS4_007.jpg/revision/latest?cb=20150304220648'},
+    {
+        id: 2,
+        name: 'Sam Winchester',
+        role: 'Back-end Engineer',
+        image: 'https://vignette.wikia.nocookie.net/supernatural/images/8/87/The_Trap_09.jpg/revision/latest?cb=20200108224218'
+    },
+    {
+        id: 3,
+        name: 'Castiel',
+        role: 'UX Designer',
+        image: 'https://vignette.wikia.nocookie.net/supernatural/images/7/7e/Castiel_807.jpg/revision/latest/scale-to-width-down/340?cb=20121027012227'
+    },
+]
+
+export default members
